07d7bbc3e08b71e2c1583ec655656191f125fc76
kelvinsIAV <kelvin@itsavirus.com>
2022-05-26 15:12:58 +0800

    Setup notification

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 pip install selenium
 pip show selenium
+pip install webdriver-manager
---
+++ tests/home/test_home_up.py
@@ -0,0 +1,44 @@
+import os
+import unittest
+import sys
+from selenium import webdriver 
+from selenium.webdriver.common.keys import Keys 
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from time import sleep
+from discord_webhook import DiscordWebhook, DiscordEmbed
+
+webhook_url = "https://discord.com/api/webhooks/841577243751088148/0aiGgeGATYNaG0hj-dFo5chxwp3Avrfq1Xj46-G_MLICX2gm_ROTKmPip8oCzuaL3YUd"
+
+
+try:
+    # driver = webdriver.Chrome("Driver/chromedriver_mac64") 
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu') 
+
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), chrome_options=options)
+    driver.get("https://consumer.burgerchain.stg.thenewfork.com") 
+    driver.maximize_window() 
+
+    try:
+        driver.find_element(by=By.XPATH, value="//h1[contains(text(),'Track all 9 ingredients')]")
+        print("Website is working")
+    except:
+        webhook = DiscordWebhook(url=webhook_url, content="Selenium test: " + driver.current_url)
+        # create embed object for webhook
+        # you can set the color as a decimal (color=242424) or hex (color='03b2f8') number
+        embed = DiscordEmbed(title='Website has problem.', description='Please Check the server', color='FF0303')
+
+        # add embed object to webhook
+        webhook.add_embed(embed)
+
+        response = webhook.execute()
+    
+    sleep(5) 
+    print(driver.title) 
+finally:
+    driver.close()
